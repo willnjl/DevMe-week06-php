ed56fe02b3d7b7b5575389c74ac376baa55632be
Will <will_leighton@outlook.com>
2020-08-04 15:49:51 +0100

    05 complete

---
+++ 04-classes/05-string.php
@@ -0,0 +1,46 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+class Stringy{
+    private $string;
+
+    public function __construct($str)
+    {
+        $this->string = $str;
+    }
+
+    public function lower()
+    {
+        return strtolower($this->string);
+    }
+    
+    public function upper(Type $var = null)
+    {
+        return strtoupper($this->string);
+    }
+
+    public function append($str)
+    {
+        return $this->string . $str;
+    }
+
+    public function repeat($n)
+    {
+        return str_repeat($this->string, $n);
+    }
+}
+
+$string = new Stringy("Na");
+
+// it can lowercase a string
+dump($string->lower()); // "na"
+
+// it can uppercase a string
+dump($string->upper()); // "NA"
+
+// it can concatenate
+dump($string->append("blah")); // "Nablah"
+
+// it can repeat a string
+dump($string->repeat(5)); // "NaNaNaNaNa"
---
+++ 04-classes/06-emails-postcode.php
@@ -0,0 +1,36 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+class Validator{
+    public function email($str)
+    {
+    return preg_match('/^\S+@[a-z]+[.][a-z]+/i', $str) === 1;
+    }
+
+    public function postcode($str)
+    {
+    return preg_match('', $str) === 1;
+    }
+}
+
+$validator = new Validator();
+
+// it validates email addresses
+echo "Email\n";
+dump($validator->email("alfonso@example.com")); // true
+dump($validator->email("wombat+12@spoons.plumbing")); // true
+dump($validator->email("blah blah blah! alfonso@example.com")); // false
+dump($validator->email("wombat@spoonsplumbing")); // false
+dump($validator->email("wombatspoons")); // false
+dump($validator->email("@wombatspoons")); // false
+
+// it validates postcodes
+echo "\nPostcode\n";
+// dump($validator->postcode("BS4 3UH")); // true
+// dump($validator->postcode("S10 4GR")); // true
+// dump($validator->postcode("BS14 9DI")); // true
+// dump($validator->postcode("SW1A 1AA")); // true
+// dump($validator->postcode("12B DI9")); // false
+// dump($validator->postcode("EST4 DD29")); // false
+// dump($validator->postcode("blah blah BS5 8RJ blah blah")); // false
