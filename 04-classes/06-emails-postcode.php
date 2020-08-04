4517bb9e7dfce0b63035b7d4050ff550ca83832d
Will <will_leighton@outlook.com>
2020-08-04 15:59:07 +0100

    challenges complete

--- 04-classes/06-emails-postcode.php
+++ 04-classes/06-emails-postcode.php
@@ -10,7 +10,7 @@ class Validator{
 
     public function postcode($str)
     {
-    return preg_match('', $str) === 1;
+    return preg_match('/^\D+\w+\s\d+\D{2}$/i', $str) === 1;
     }
 }
 
@@ -27,10 +27,10 @@ dump($validator->email("@wombatspoons")); // false
 
 // it validates postcodes
 echo "\nPostcode\n";
-// dump($validator->postcode("BS4 3UH")); // true
-// dump($validator->postcode("S10 4GR")); // true
-// dump($validator->postcode("BS14 9DI")); // true
-// dump($validator->postcode("SW1A 1AA")); // true
-// dump($validator->postcode("12B DI9")); // false
-// dump($validator->postcode("EST4 DD29")); // false
-// dump($validator->postcode("blah blah BS5 8RJ blah blah")); // false
+dump($validator->postcode("BS4 3UH")); // true
+dump($validator->postcode("S10 4GR")); // true
+dump($validator->postcode("BS14 9DI")); // true
+dump($validator->postcode("SW1A 1AA")); // true
+dump($validator->postcode("12B DI9")); // false
+dump($validator->postcode("EST4 DD29")); // false
+dump($validator->postcode("blah blah BS5 8RJ blah blah")); // false
